7f3775ea19b2796f2b430786c7e826cb38b4d432
EddieHermoza <eddie.ehc04@gmail.com>
2025-03-21 19:58:48 -0500

    feat: enhance layout and add registration page; update button styles and loading state

--- src/app/(barbershop)/auth/layout.tsx
+++ src/app/(barbershop)/auth/layout.tsx
@@ -1,4 +1,4 @@
 import React from 'react'
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <main className="">{children}</main>
+  return <main className="w-full h-screen flex-center">{children}</main>
 }
