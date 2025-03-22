ef55d908da4fa6570f17a0585995446bee10da9b
EddieHermoza <eddie.ehc04@gmail.com>
2025-03-21 19:25:32 -0500

    feat: add react-hook-form and react-icons dependencies; implement sign-in form layout

--- src/app/(barbershop)/auth/layout.tsx
+++ src/app/(barbershop)/auth/layout.tsx
@@ -1,8 +1,4 @@
 import React from 'react'
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <main className=" max-sm:h-[calc(100vh-180px)] h-[calc(100vh-100px)] w-full flex-center flex-col gap-2">
-      {children}
-    </main>
-  )
+  return <main className="">{children}</main>
 }
--- src/app/(barbershop)/auth/signIn/page.tsx
+++ src/app/(barbershop)/auth/signIn/page.tsx
@@ -1,7 +1,56 @@
+'use client'
+import Link from 'next/link'
+import { useState } from 'react'
+import { AiOutlineLoading } from 'react-icons/ai'
+
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+
 export default function Page() {
+  const [loading, setLoading] = useState(false)
+
   return (
-    <div>
-      <h1>Hello Page</h1>
-    </div>
+    <>
+      <Card className="max-w-md w-full">
+        <CardHeader>
+          <CardTitle className="text-center text-2xl sm:text-3xl">
+            Iniciar Sesión
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="grid w-full items-center gap-10">
+              <label htmlFor="name" className="flex flex-col gap-2">
+                <span>Correo Electrónico</span>
+                <Input id="email" />
+              </label>
+
+              <label htmlFor="password" className="flex flex-col gap-2">
+                <span>Contraseña</span>
+                <Input id="password" type="password" />
+              </label>
+
+              <div className="w-full flex-center flex-col gap-2 relative">
+                <Button asChild variant={'outline'} className="text-xl w-full">
+                  <Link href={'/auth/register'}>Registrarse</Link>
+                </Button>
+                o
+                <Button disabled={loading} className="text-xl w-full">
+                  {loading ? (
+                    <AiOutlineLoading
+                      size={18}
+                      className="animate-spin ease-in-out"
+                    />
+                  ) : (
+                    <>Iniciar Sesión</>
+                  )}
+                </Button>
+              </div>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </>
   )
 }
